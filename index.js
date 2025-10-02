93957f16954b81049c0b3194534da84aef3d7f62
Alexandros Iosifidis <196984481+LozFunk@users.noreply.github.com>
2025-10-02 15:29:21 +0200

    beginning of project

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';  
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on http://localhost:3000");
+});
